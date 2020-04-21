67a91218ccdee4cd90e0bfed918b77bbb05331e0
Brijesh Kumar <brij10718@gmail.com>
2020-04-21 14:46:55 +0530

    js1-week3 problem-1

---
+++ javascript/javascript1/week3/Problem-1/index.js
@@ -0,0 +1,16 @@
+// Item array removal
+//Remove the item in names that is equal to nameToRemove
+
+const names = ['Peter', 'Ahmad', 'Yana', 'kristina',  'Rasmus', 
+'Samuel', 'katrine', 'Tala'];
+const nameToRemove = 'Ahmad';
+
+for (i = 0; i < names.length; i++ ) {
+  if (names[i] === nameToRemove) {
+    names.splice(i, 1);
+    
+  }
+
+}
+
+console.log(names);
