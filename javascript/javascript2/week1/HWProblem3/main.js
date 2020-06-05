d0d4e0ef054cd692ab8c763fd97f3a4462dc0782
Brijesh Kumar <brij10718@gmail.com>
2020-06-05 17:47:18 +0530

    updated the code

--- javascript/javascript2/week1/HWProblem3/main.js
+++ javascript/javascript2/week1/HWProblem3/main.js
@@ -7,7 +7,7 @@ function renderProducts(products) {
   const ul = document.querySelector('ul');
   products.forEach((product) => {
     const li = document.createElement('li');
-    li.innerHTML = `<h2>${product.name}</h2><span>price: ${product.price}</span><br><span>Rating: ${product.rating}</span>`;
+    li.innerHTML = `<h2>${product.name}</h2><span>Price: ${product.price}</span><br><span>Rating: ${product.rating}</span>`;
     ul.appendChild(li);
   });
 }
